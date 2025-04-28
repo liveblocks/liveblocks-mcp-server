7db5ca2f6db84befecb551ee354ec75254be2c28
Chris Nicholas <ctnicholasdev@gmail.com>
2025-04-28 16:36:30 +0100

    feat: Real schema doesnt work

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,58 @@ export const server = new McpServer({
   },
 });
 
+/*
+ Zod does not support tuple or literal here
+ Realistic APIs we could support
+ getRooms no args
+
+*/
+
+const RoomPermission = z.union([
+  z.tuple([z.literal("room:write")]),
+  z.tuple([z.literal("room:read"), z.literal("room:presence:write")]),
+]);
+
+const RoomPermissionWithEmpty = z.union([
+  z.tuple([z.literal("room:write")]),
+  z.tuple([z.literal("room:read"), z.literal("room:presence:write")]),
+  z.tuple([]),
+]);
+
+server.tool(
+  "liveblocks-create-room",
+  "Create a Liveblocks room",
+  {
+    roomId: z.string(),
+    defaultAccesses: RoomPermissionWithEmpty,
+    groupsAccesses: z.record(z.string(), RoomPermission),
+    usersAccesses: z.record(z.string(), RoomPermission),
+    metadata: z.record(z.string(), z.string()).optional(),
+  },
+  async ({ roomId }, extra) => {
+    return await callLiveblocksApi(
+      liveblocks.createRoom(
+        roomId,
+        { defaultAccesses: ["room:write"] },
+        { signal: extra.signal }
+      )
+    );
+  }
+);
+
+server.tool(
+  "liveblocks-get-rooms",
+  "Get recent Liveblocks rooms",
+  {
+    limit: z.number(),
+  },
+  async ({ limit }, extra) => {
+    return await callLiveblocksApi(
+      liveblocks.getRooms({ limit }, { signal: extra.signal })
+    );
+  }
+);
+
 server.tool(
   "liveblocks-get-room",
   "Get a Liveblocks room",
